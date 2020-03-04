ab01204985657d169d6f48b8bfbac1660de83b97
Mantou <49581173+Matoutou@users.noreply.github.com>
2020-03-05 00:16:37 +0800

    优化table中对某值进行转义代码

--- src/views/mine-components/components/step/step-content1.vue
+++ src/views/mine-components/components/step/step-content1.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="pane">
     <h1>第一个步骤内容</h1>
+    <div>
+      <el-table :data="tableData" border stripe>
+        <el-table-column label="用户姓名" prop="userName" />
+        <el-table-column label="类型值">
+          <template slot-scope="scope">{{ typeDesc(scope.row.typeCode) }}</template>
+        </el-table-column>
+      </el-table>
+    </div>
     <div style="text-align:center;margin-top:18px;">
       <el-button type="primary" @click="handleNextStep()">下一步</el-button>
     </div>
@@ -12,7 +20,22 @@ export default {
   name: 'StepContent1',
   data() {
     return {
-
+      tableData: [
+        { userName: '张三', typeCode: '1' },
+        { userName: '李四', typeCode: '2' }
+      ],
+      typeOption: [
+        { label: '类型一', value: '1' },
+        { label: '类型二', value: '2' }
+      ]
+    }
+  },
+  computed: {
+    typeDesc(val) {
+      return (val) => {
+        const target = this.typeOption.find(i => i.value === val)
+        return target ? target.label : ''
+      }
     }
   },
   methods: {
